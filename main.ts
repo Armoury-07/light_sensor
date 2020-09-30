ae4c9cda9f593e12e5c3c3c6c2c13dc4bbb2b7b1
Armoury-07 <70344963+Armoury-07@users.noreply.github.com>
2020-09-30 14:55:57 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let Light = 0
+basic.forever(function () {
+    Light = input.lightLevel()
+    if (Light >= 0 && Light <= 85) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Light >= 86 && Light <= 170) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
